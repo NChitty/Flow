bd1881ba7fca02025967b51e486f903195e8cef1
Nicholas Chitty <nicholas.chitty@gmail.com>
2022-10-25 18:57:17 -0400

    Created command-line tool with all necessary functions

--- main.py
+++ main.py
@@ -4,74 +4,90 @@ from objects.bdd import BDD, read_bdd
 from objects.crossbar import Crossbar, read_crossbar
 
 
-def performance_comparison_enumeration(n_vars):
-    bdd = read_bdd(f"bdds/var{n_vars}.bdd")
-    xbar = read_crossbar(f"xbars/var{n_vars}.xbar")
-    start = time.process_time_ns()
-    print(f"{n_vars} Variables: {bdd.enumeration_verification(xbar)}")
-    end = time.process_time_ns()
-    print(f"\t\t{(end - start) / 1000.0}ms")
+def parse_command(cmd_str: str):
+    return cmd_str.split()
 
 
-def performance_comparison_synthesis(n_vars):
-    bdd = read_bdd(f"bdds/var{n_vars}.bdd")
-    start = time.process_time_ns()
-    xbar = bdd.synthesize_xbar()
-    end = time.process_time_ns()
-    print(f"Nodes {len(bdd.nodes)}: {(end-start)/1000.0}ms")
+def parse_index(cmd_arr, val_from):
+    val = -1
+    if len(cmd_arr) > val_from:
+        val = int(cmd_arr[val_from])
+    return val
 
 
-def test_equivalence(no_of_tests):
-    bdd = read_bdd("bdds/var5.bdd")
-    print(f"{'Test':4}|Equivalent")
-    for x in range(1, no_of_tests + 1):
-        xbar = read_crossbar(f"xbars/test{x}.xbar")
-        print(f"{x:4}|{'True' if bdd.enumeration_verification(xbar) == 0 else 'False':10}")
+def help_cmd():
+    print("<required argument> [optional argument]")
+    print("read <file> - Reads the file using the format of the given (.bdd or .xbar)")
+    print("list <bdd|xbar> - lists the loaded xbars or bdds")
+    print("print <bdd|xbar> [item number in list] - Prints the file format to the screen,\n"
+          "if no number is given, it will print the most recent loaded object")
+    print("tt <bdd|xbar> [item number in list] - Prints the truth table for the most recent\n"
+          "xbar or bdd or the object in the numbered list")
+    print("verenum <bdd #> <xbar #> - Verify the equivalence of a bdd and xbar via enumeration")
+    print("synthesize <filename> [bdd #] - Synthesizes a compact crossbar from a bdd and pushes the result to the top of the list")
+    print("save <xbar #> <filename> - Save the xbar to the specified file name")
 
 
-def lone_equivalence_test(bdd_in, xbar_in):
-    if type(bdd_in) is BDD:
-        bdd = bdd_in
-    else:
-        bdd = read_bdd(bdd_in)
-    if type(xbar_in) is Crossbar:
-        xbar = xbar_in
-    else:
-        xbar = read_crossbar(xbar_in)
-    value_list = bdd.enumeration_verification(xbar)
-    if value_list:
-        for x in range(len(bdd.variables)):
-            print(f'{("x_" + str(x+1)):4}|', end='')
-        print(f'{"bdd":4}|{"xbar":4}|')
-        for x in value_list:
-            for y in x:
-                print(f'{1 if y else 0:4}|', end='')
-            print()
-        return
-    print(True)
-
-
-def synthesis_test(bdd_file):
-    bdd = read_bdd(bdd_file)
-    xbar = bdd.synthesize_xbar()
-    if not len(bdd.enumeration_verification(xbar)) == 0:
-        string = bdd_file
-        print(f"Creating synthesized xbar file @ xbars/{string.split('/')[1].split('.')[0]}_synthesis.xbar")
-        xbar.fprint_matrix(f"xbars/{string.split('/')[1].split('.')[0]}_synthesis.xbar")
-    else:
-        print("Synthesis: Success.")
-
-
-def synthesize_xbar(bdd_file):
-    bdd = read_bdd(bdd_file)
-    xbar = bdd.synthesize_xbar()
-    string = bdd_file
-    print(f"Creating synthesized xbar file @ xbars/{string.split('/')[1].split('.')[0]}_synthesis.xbar")
-    xbar.fprint_matrix(f"xbars/{string.split('/')[1].split('.')[0]}_synthesis.xbar")
+def list_obj(data: list):
+    for i in range(len(data)):
+        if type(data[i]) is BDD:
+            print(f"[{i}] {data[i].file}")
+        elif type(data[i]) is Crossbar:
+            print(f"[{i}] {data[i].file}")
 
 
 if __name__ == "__main__":
-    xbar = read_crossbar("xbars/var2.xbar")
-    xbar.truth_table()
-    bdd = read_bdd(("bdds/var2.bdd"))
-    bdd.truth_table()
+    cmd = input(">>> ")
+    is_bdd = ["bdd", "bdds"]
+    is_xbar = ["xbar", "xbars"]
+    bdds = []
+    xbars = []
+    while cmd != "exit":
+        parsed_command = parse_command(cmd.lower())
+        if parsed_command[0] == "read":
+            if parsed_command[1].split('.')[-1] == "bdd":
+                bdds.append(read_bdd(parsed_command[1]))
+            elif parsed_command[1].split('.')[-1] =="xbar":
+                xbars.append(read_crossbar(parsed_command[1]))
+        elif parsed_command[0] == "list":
+            if len(parsed_command) < 2:
+                print("List document necessary")
+                continue
+            if parsed_command[1] in is_xbar:
+                list_obj(xbars)
+            elif parsed_command[1] in is_bdd:
+                list_obj(bdds)
+        elif parsed_command[0] == "print":
+            index = parse_index(parsed_command, 2)
+            if parsed_command[1] in is_xbar:
+                xbars[index].print_matrix()
+            elif parsed_command[1] in is_bdd:
+                bdds[index].print()
+        elif parsed_command[0] == "tt":
+            index = parse_index(parsed_command, 2)
+            if parsed_command[1] in is_bdd:
+                bdds[index].truth_table()
+            elif parsed_command[1] in is_xbar:
+                xbars[index].truth_table()
+        elif parsed_command[0] == "verenum":
+            if len(parsed_command) < 3:
+                print("Need a value for the desired bdd and xbar.")
+                continue
+            index_bdd = int(parsed_command[1])
+            index_xbar = int(parsed_command[2])
+            is_equivalent = bdds[index_bdd].enumeration_verification(xbars[index_xbar])
+            if len(is_equivalent) == 0:
+                print(f"BDD {index_bdd} is equivalent to Crossbar {index_xbar}")
+            else:
+                print(f"BDD {index_bdd} is not equivalent to Crossbar {index_xbar}")
+        elif parsed_command[0] == "synthesize":
+            index = parse_index(parsed_command, 2)
+            xbars.append(bdds[index].synthesize_xbar(parsed_command[1]))
+        elif parsed_command[0] == "save":
+            index = parse_index(parsed_command, 1)
+            xbars[index].file = parsed_command[2]
+            xbars[index].fprint_matrix()
+        else:
+            help_cmd()
+
+        cmd = input(">>> ")
